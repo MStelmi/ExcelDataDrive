56de571a6cc9873a8383997808174b56bbe773ab
MStelmi <michala.stelmach@gmail.com>
2019-09-16 14:01:11 +0200

    excel data driven for register test

--- src/test/java/UI/tests/Register.java
+++ src/test/java/UI/tests/Register.java
@@ -1,15 +1,30 @@
 package UI.tests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
+import utility.Excel;
 
 public class Register extends Base{
+
+    Excel excel;
+    String fileName = "file.csv";
+    String user, email, password;
+
+    @BeforeEach
+    public void setup()
+    {
+        excel = new Excel (fileName);
+        user = excel.getSpecificCellValue(1, "username");
+        email = excel.getSpecificCellValue(1, "email");
+        password = excel.getSpecificCellValue(1, "password");
+    }
 
     @Test
     public void registerNewUser(){
         home.navigateToRegistry();
         Assert.assertTrue(registry.isRegistryPagePresent());
-        registry.setCredentials("stelmi", "stelmi@mail.com", "123456");
+        registry.setCredentials(user,email,password);
         registry.submitButton.click();
     }
 }
